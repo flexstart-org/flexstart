020abbf877a673317320e07cb70e0b6222390253
Mitesh Gupta <miteshgupta15@gmail.com>
2025-02-16 05:00:22 +0530

    Add authentication middleware, update TypeScript configuration, and enhance Tailwind CSS setup

--- apps/shop/lib/utils.ts
+++ apps/shop/lib/utils.ts
@@ -1,11 +1,22 @@
-import { ReadonlyURLSearchParams } from 'next/navigation';
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { ReadonlyURLSearchParams } from "next/navigation";
 
-export const createUrl = (pathname: string, params: URLSearchParams | ReadonlyURLSearchParams) => {
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export const createUrl = (
+  pathname: string,
+  params: URLSearchParams | ReadonlyURLSearchParams
+) => {
   const paramsString = params.toString();
-  const queryString = `${paramsString.length ? '?' : ''}${paramsString}`;
+  const queryString = `${paramsString.length ? "?" : ""}${paramsString}`;
 
   return `${pathname}${queryString}`;
 };
 
 export const ensureStartsWith = (stringToCheck: string, startsWith: string) =>
-  stringToCheck.startsWith(startsWith) ? stringToCheck : `${startsWith}${stringToCheck}`;
+  stringToCheck.startsWith(startsWith)
+    ? stringToCheck
+    : `${startsWith}${stringToCheck}`;
